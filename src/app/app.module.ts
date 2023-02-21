687dc943b018a14c18d00e3dd95830def4b8d280
Robert <robert.ivan.saltos@hotmail.com>
2023-02-20 21:55:46 -0500

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,15 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ContadorComponent } from './Contador/Contador.Component';
+import { HeroeComponent } from './heroes/heroe/heroe.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ContadorComponent,
+    HeroeComponent
+    
   ],
   imports: [
     BrowserModule,
